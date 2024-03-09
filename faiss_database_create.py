23d9cb51c5fc32ee23e1382f6e8b8368ae53466e
handsomexiu <1184817330@qq.com>
2024-03-09 17:02:51 +0800

    取消了IndexIDMap

--- faiss_database_create.py
+++ faiss_database_create.py
@@ -17,18 +17,19 @@ def data_piece(data_choice:str='glove',n_piece:int=5,dim:int=25)->dict:# 如果d
     length_all = len(glove_hdf['train'])# 获取数据的总长度  
     cut_point=int(length_all/n_piece)# 获取切分点,int是向下取整
     data_dict={}# 这里用字典来存取每一段切分点
-    id_dict={}# 这里用字典来存取每一段对应的索引
+    # id_dict={}# 这里用字典来存取每一段对应的索引
     for i in range(n_piece):
         data_key=data_name+'_n'+str(n_piece)+'_'+str(i+1)
         if i+1 == n_piece:
             data_dict[data_key]=glove_hdf['train'][i*cut_point:]# 把剩下的余数全部纳入不在单独考虑了（如果能整除则恰好每一块长度相同，如果不能整除则最后一段的长度=cut_point+余数），这里是最后一段，
-            id=list(range(i*cut_point,length_all))
-            id_dict[data_key]=id
+            # id=list(range(i*cut_point,length_all))
+            # id_dict[data_key]=id
         else:
             data_dict[data_key]=glove_hdf['train'][i*cut_point:(i+1)*cut_point]
-            id=list(range(i*cut_point,(i+1)*cut_point))
-            id_dict[data_key]=id
-    return data_dict,id_dict,data_name
+            # id=list(range(i*cut_point,(i+1)*cut_point))
+            # id_dict[data_key]=id
+    # return data_dict,id_dict,data_name
+    return data_dict,data_name
 
 # 创建向量数据库所需要的文件夹
 def create_folder_if_not_exists(data_name:str,n_piece:int=5)->str:#这里的n_piece需要与data_piece函数中的n_piece一致
@@ -46,7 +47,8 @@ def create_folder_if_not_exists(data_name:str,n_piece:int=5)->str:#这里的n_pi
 # 关于向量数据库所需要的一些个参数也可以设为该函数需要传的参数如measure, param
 # 这里的关于分片后的数据的id还得自定义一下
 def create_index(data_choice:str='glove',n_piece:int=5,dim:int=25,param:str='HNSW64'):# 如果data_choice是glove则dim必须是25,50,100,200，如果是sift则dim必须是128，否则会报错
-    data_dict,id_dict,data_name=data_piece(data_choice,n_piece,dim)
+    # data_dict,id_dict,data_name=data_piece(data_choice,n_piece,dim)
+    data_dict,data_name=data_piece(data_choice,n_piece,dim)
     folder_path = create_folder_if_not_exists(data_name,n_piece)
     for i in range(n_piece):
         data_key=data_name+'_n'+str(n_piece)+'_'+str(i+1) # 我们统一这样命名
@@ -60,13 +62,13 @@ def create_index(data_choice:str='glove',n_piece:int=5,dim:int=25,param:str='HNS
         if data_choice=='glove':
             print(f'现在处理的glove数据集{data_key},需要进行归一化处理')
             faiss.normalize_L2(df)# glove数据采用的angular距离，首先需要进行归一化,然后再进行faiss.METRIC_L2
-        # index.add(df)
-        # faiss.write_index(index, file_path)
-        # 做id的映射和自定义
-        xids = np.array(id_dict[data_key])
-        IDMap_index = faiss.IndexIDMap(index)
-        IDMap_index.add_with_ids(df, xids)
-        faiss.write_index(IDMap_index, file_path)
+        index.add(df)
+        faiss.write_index(index, file_path)
+        # # 做id的映射和自定义
+        # xids = np.array(id_dict[data_key])
+        # IDMap_index = faiss.IndexIDMap(index)
+        # IDMap_index.add_with_ids(df, xids)
+        # faiss.write_index(IDMap_index, file_path)
         print(f"{data_key}.index 已创建。")
 
 if __name__ == "__main__":
